2ade9326fec72bc9fc6d53cfcc5fa7c9e83d991e
totalintelli <blueintelli@gmail.com>
2018-05-27 17:27:15 +0900

    테스트 프로젝트를 추가.

---
+++ TaxCalculatorTeset/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaxCalculatorTeset")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaxCalculatorTeset")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30a0ca9c-4642-4445-b86f-85faa3959c44")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaxCalculatorTeset/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using TaxCalculator;
+
+namespace TaxCalculatorTest
+{
+    [TestFixture]
+    public class TaxCalculatorTest
+    {
+        [Test]
+        public void When2013_ShouldRetrun90Percent()
+        {
+            // Arrange
+            var taxHelper = new TaxHelper(TaxYear.Year2013);
+            const int salaryExpected = 900;
+
+            // Act
+            var salaryResulted = taxHelper.Calculate(1000);
+
+            // Assert
+            Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
+        }
+
+        [Test]
+        public void When2014_ShouldReturn80Percent()
+        {
+            // Arrange
+            var taxHelper = new TaxHelper(TaxYear.Year2014);
+            const int salaryExpected = 800;
+
+            // Aact
+            var salaryResulted = taxHelper.Calculate(1000);
+
+            // Assert
+            Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
+        }
+    }
+}
